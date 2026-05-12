6251db2a23c8cc4e2f06af98f1e360609f51d8ee
meduardacosta65-crypto <meduardacosta65@gmail.com>
2026-05-11 21:46:44 -0300

    Create streamlit_app.py
    Criando estrutura inicial do Streamlit

---
+++ app/streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Passos Mágicos",
+    layout="wide"
+)
+
+st.title("🎓 Passos Mágicos")
+
+st.write("Sistema de monitoramento educacional")
